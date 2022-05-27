acdab1eefdf3a6d07c44618b7cf1c74a61ed0bac
enamul-haque <mdenamulhaque881@gmail.com>
2022-05-27 06:32:38 +0600

    added some file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.auto-type', {
-    strings: ['Md Enamul Haque', 'Font-end Developer'],
+    strings: ['Md Enamul Haque', 'Mes: Jahanara Begum' ,'Font-end Developer'],
     typeSpeed: 50,
     backSpeed: 50,
     loop: true
